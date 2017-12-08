5ac8c1e6dc0939378fbfaae358edbf6d31d8e294
Géraldine MUSAT <32127122+GeraldineMusat@users.noreply.github.com>
2017-12-08 10:36:52 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,39 @@
-// TO DO
+window.onload=init;
+var gf;
+
+function init(){
+  gf = new GameFramework();
+  gf.init();
+}
+
+function GameFramework(){
+    let canvas, ctx, w, h;
+    let tableauObjetGraphiques=[];
+
+    function init() {
+        canvas=document.querySelector("#myCanvas");
+        ctx=canvas.getContext("2d");
+        w=canvas.width;
+        h=canvas.height;
+        requestAnimationFrame(anime);
+    }
+
+    function anime(timeElapsed){
+        ctx.clearRect(0,0,w,h);
+        tableauObjetGraphiques.forEach(function(r){
+          r.draw(ctx);
+          r.move();
+          r.testCollisionZone(w,h);
+        });
+        requestAnimationFrame(anime);
+    }
+
+    function reset() {
+        tableauObjetGraphiques=[];
+    }
+
+    return {
+        init:init,
+        reset:reset
+      }
+}
