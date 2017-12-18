73e88325ba890abd3fac20db12e62dbabb3d8260
Géraldine MUSAT <32127122+GeraldineMusat@users.noreply.github.com>
2017-12-18 10:08:47 +0100

    Ajout Map
    Ajout d'une Map du hall d'entree de la Miage (fait avec Paint), changement de cette map quand on arrive en haut à droit du canvas

--- javascript.js
+++ javascript.js
@@ -14,24 +14,20 @@ function GameFramework(){
     window.onkeydown = function(event) {
         switch (event.keyCode || event.which) {
             case 37:
-            console.log("gauche");
+            //console.log("gauche");
             dir_event = DIRECTION.GAUCHE;
-            //prochaineCase = this.getPosition(this.direction);
             break;
         case 39:
-            console.log("droite");
+            //console.log("droite");
             dir_event = DIRECTION.DROITE;
-            //prochaineCase = this.getPosition(this.direction);
             break;
         case 38:
-            console.log("haut");
+            //console.log("haut");
             dir_event = DIRECTION.HAUT;
-            //prochaineCase = this.getPosition(this.direction);
             break;
         case 40:
-            console.log("bas");
+            //console.log("bas");
             dir_event = DIRECTION.BAS;
-            //prochaineCase = this.getPosition(this.direction);
             break;
         default:
             break;
@@ -44,14 +40,14 @@ function GameFramework(){
         ctx=canvas.getContext("2d");
         this.w=canvas.width;
         this.h=canvas.height;
-        console.log("dans le init");
+        //console.log("dans le init");
         requestAnimationFrame(anime);
     }
 
     function anime(timeElapsed){
         tableauObjetGraphiques.forEach(function(r){
           r.draw(ctx);
-          console.log("dans le anime");
+          //console.log("dans le anime");
           r.move();
           //r.testCollisionZone(w,h);
         });
@@ -59,7 +55,7 @@ function GameFramework(){
     }
 
     function creerPersonnage(){
-        console.log("Dans creer perso");
+        //console.log("Dans creer perso");
         let perso = new Personnage(this.w/2, this.h/2);//posX et posY
         tableauObjetGraphiques.push(perso);
     }
@@ -89,18 +85,34 @@ class ObjetGraphique {
         this.width = largeur;
         this.height = hauteur;
         this.direction = dir_event;
+        this.map = new Map('Hall_entree.png');
     }
 
     draw(ctx){
-        ctx.save();
-        
-        ctx.restore();
+    }
+
+    getZone_changeMap() {
+        var perso = {x: this.x, y: this.y, width: this.width, height: this.width}; //met a jour les coordonées du perso
+        var zone = {x: 2, y: 50, width: 30, height: 30};
+
+        if(this.map.img === 'Hall_entree.png'){
+            if (perso.x < zone.x + zone.width && perso.x + perso.width > zone.x && perso.y < zone.y + zone.height && perso.height + perso.y > zone.y) {
+                // zone détectée !
+               this.map.change_map("Map_sol.png");
+           }
+        }
+    }
+
+    testCollision() {
+
     }
 
     getPosition(direction) {
         var coord = {'x' : this.x, 'y' : this.y};
-        console.log("dans le get Position");
-        this.direction = dir_event;
+        console.log("x = "+this.x);
+        console.log("y = "+this.y);
+        //console.log("dans le get Position");
+        this.getZone_changeMap();
         switch(direction){
             case DIRECTION.BAS : 
                 coord.y++;
@@ -121,11 +133,12 @@ class ObjetGraphique {
     }
 
     move() {
-        console.log("dans le move de onObjetGraphique");
+        //console.log("dans le move de onObjetGraphique");
         
         var prochaineCase = this.getPosition(dir_event);
         //on effectue le deplacement 
-        
+        console.log(dir_event);
+       
         this.x = prochaineCase.x;
         this.y = prochaineCase.y;
 
@@ -150,14 +163,44 @@ class Personnage extends ObjetGraphique {
     draw(ctx){
         ctx.save();
         ctx.clearRect(0,0,300, 300);
+        //this.map.draw_item(ctx);
         this.dessineCorps(ctx);
-        //super.draw(ctx);
         ctx.restore();
     }
 
     move() {
-        console.log("dans le move de Personnage");
+        //console.log("dans le move de Personnage");
         super.move();
+    }
+}
+
+class Map {
+    constructor (chemin_image) {
+        //console.log("dans le constructeut de map")
+        //Elle aura un nom/lien image
+        //Elle aura un tableau pour les collisions
+        this.img = chemin_image;
+        this.draw_map(this.img);
+    }
+
+    draw_item(ctx) {
+        //console.log("dans draw tile");
+        //var img = new Image();
+        //img.onload = function() {
+            //ctx.drawImage(img, 0, 0);
+        //};
+        //img.src = 'testTileSet.png';
+    }
+
+    draw_map(img) {
+        var attribute = "background: url("+img+")";
+        var canvas = document.getElementsByTagName('canvas')[0];
+        canvas.setAttribute('style', attribute);
+    }
+
+    change_map(img){
+        this.img = img;
+        this.draw_map(this.img);
     }
 }
 
@@ -166,7 +209,3 @@ function initialisationPerso() {
     gf.creerPersonnage();
 }
 
-function initialisationPerso2() {
-    //print("Hello World !");
-    document.writeln("Hello world !");
-}
